903af4dae5152f7617b67c16235f4976530f86ea
fill-storage <216758177+fill-storage@users.noreply.github.com>
2025-06-18 21:53:34 +0545

    Fix Hydration issue

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -4,20 +4,35 @@ import "../globals.css";
 import { NextIntlClientProvider } from "next-intl";
 import Header from "@/component/Header";
 import Footer from "@/component/Footer";
+import { Geist, Geist_Mono } from "next/font/google";
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export default async function RootLayout({
   children,
   params,
 }: {
   children: React.ReactNode;
-  params: Promise <{ locale: string }>;
+  params: Promise<{ locale: string }>;
 }) {
   // Do NOT fallback to "en" here, let Next.js handle the params
   const messages = (await import(`../../messages/${(await params).locale}.json`)).default;
 
+  
   return (
-    <html lang={(await params).locale}>
-      <body>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         <NextIntlClientProvider locale={(await params).locale} messages={messages}>
           <Header />
           <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
